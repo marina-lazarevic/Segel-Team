c12954d71de2ef5cd5f59858e1b86b30d2a0a4ba
Marina <lazarevic.ui@gmail.com>
2022-08-05 22:07:49 +0200

    about responsive

--- src/js/about.js
+++ src/js/about.js
@@ -6,7 +6,9 @@ json.forEach(founder => {
     let {name, role, bio, image} = founder;
     about_slider.innerHTML +=
     `<div class="slider__slide">
-     <img src="${image}" alt="${name} - ${role}" class="slider__img">
+        <div class="slider__img-container">
+            <img src="${image}" alt="${name} - ${role}" class="slider__img">
+        </div>
         <div class="slider__about-info">
             <h3 class="slider__name">${name}</h3>
             <h4 class="slider__role">${role}</h4>
